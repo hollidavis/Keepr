501d50c380c659f799a8f3346eb714d4250d7955
Holli Davis <hollidavis19@gmail.com>
2021-09-10 11:14:41 -0600

    added auth/database info

--- Keepr.client/src/env.js
+++ Keepr.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'holli-codeworks.us.auth0.com'
+export const audience = 'https://keepr.com'
+export const clientId = 'BCpSMV4FD4O9bhZV8ItN6JKS5JRDUasB'
