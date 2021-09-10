f8fb2af1222f90dd02438ab5f0885213f77b3b71
Holli Davis <hollidavis19@gmail.com>
2021-09-10 11:41:38 -0600

    accounts working

--- Keepr.client/src/env.js
+++ Keepr.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:5001' : ''
 export const domain = 'holli-codeworks.us.auth0.com'
 export const audience = 'https://keepr.com'
 export const clientId = 'BCpSMV4FD4O9bhZV8ItN6JKS5JRDUasB'
