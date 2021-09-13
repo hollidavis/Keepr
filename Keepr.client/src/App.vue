7da13627b26ed691d26378559aef00a6bda619ef
Holli Davis <hollidavis19@gmail.com>
2021-09-13 16:21:29 -0600

    footer

--- Keepr.client/src/App.vue
+++ Keepr.client/src/App.vue
@@ -6,8 +6,8 @@
     <router-view />
   </main>
   <footer>
-    <div class="bg-dark text-light text-center p-4">
-      Made with 💖 by CodeWorks
+    <div class="bg-primary text-light text-center p-3">
+      Made with 💖 by Holli
     </div>
   </footer>
 </template>
